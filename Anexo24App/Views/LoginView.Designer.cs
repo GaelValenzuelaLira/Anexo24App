48ce07c6f6337adcf93cc32377d9be89ebb14582
emes <emes@ERNESTO>
2026-04-06 15:22:32 -0600

    Avances del login 3

--- Anexo24App/Views/LoginView.Designer.cs
+++ Anexo24App/Views/LoginView.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.lblTitulo = new System.Windows.Forms.Label();
             this.lblSubtitulo = new System.Windows.Forms.Label();
+            this.panelCredenciales = new System.Windows.Forms.Panel();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnLoggin = new System.Windows.Forms.Button();
+            this.lblRecuperar = new System.Windows.Forms.Label();
+            this.panelCredenciales.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -37,9 +45,10 @@
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
             this.lblTitulo.Font = new System.Drawing.Font("Trebuchet MS", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(294, 20);
+            this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lblTitulo.Location = new System.Drawing.Point(208, 5);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(412, 46);
+            this.lblTitulo.Size = new System.Drawing.Size(334, 38);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Sistema de Inventarios";
             // 
@@ -49,23 +58,115 @@
             this.lblSubtitulo.BackColor = System.Drawing.Color.Transparent;
             this.lblSubtitulo.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSubtitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblSubtitulo.Location = new System.Drawing.Point(315, 75);
+            this.lblSubtitulo.Location = new System.Drawing.Point(232, 40);
             this.lblSubtitulo.Name = "lblSubtitulo";
-            this.lblSubtitulo.Size = new System.Drawing.Size(370, 26);
+            this.lblSubtitulo.Size = new System.Drawing.Size(285, 22);
             this.lblSubtitulo.TabIndex = 1;
             this.lblSubtitulo.Text = "Ingresa tus credenciales para continuar";
             // 
+            // panelCredenciales
+            // 
+            this.panelCredenciales.BackColor = System.Drawing.Color.White;
+            this.panelCredenciales.Controls.Add(this.lblRecuperar);
+            this.panelCredenciales.Controls.Add(this.btnLoggin);
+            this.panelCredenciales.Controls.Add(this.textBox1);
+            this.panelCredenciales.Controls.Add(this.lblPass);
+            this.panelCredenciales.Controls.Add(this.txtUser);
+            this.panelCredenciales.Controls.Add(this.lblCorreo);
+            this.panelCredenciales.Location = new System.Drawing.Point(212, 87);
+            this.panelCredenciales.Name = "panelCredenciales";
+            this.panelCredenciales.Size = new System.Drawing.Size(327, 332);
+            this.panelCredenciales.TabIndex = 2;
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.BackColor = System.Drawing.Color.Transparent;
+            this.lblCorreo.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreo.ForeColor = System.Drawing.Color.Black;
+            this.lblCorreo.Location = new System.Drawing.Point(20, 30);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(214, 22);
+            this.lblCorreo.TabIndex = 0;
+            this.lblCorreo.Text = "Usuario o correo electrónico";
+            // 
+            // txtUser
+            // 
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUser.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.ForeColor = System.Drawing.Color.Black;
+            this.txtUser.Location = new System.Drawing.Point(24, 57);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(281, 36);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.TabStop = false;
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.BackColor = System.Drawing.Color.Transparent;
+            this.lblPass.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.ForeColor = System.Drawing.Color.Black;
+            this.lblPass.Location = new System.Drawing.Point(20, 122);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(88, 22);
+            this.lblPass.TabIndex = 2;
+            this.lblPass.Text = "Contraseña";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(24, 149);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '#';
+            this.textBox1.Size = new System.Drawing.Size(281, 36);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TabStop = false;
+            // 
+            // btnLoggin
+            // 
+            this.btnLoggin.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLoggin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoggin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoggin.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoggin.ForeColor = System.Drawing.Color.White;
+            this.btnLoggin.Location = new System.Drawing.Point(24, 254);
+            this.btnLoggin.Name = "btnLoggin";
+            this.btnLoggin.Size = new System.Drawing.Size(281, 46);
+            this.btnLoggin.TabIndex = 4;
+            this.btnLoggin.Text = "Iniciar sesión";
+            this.btnLoggin.UseVisualStyleBackColor = false;
+            // 
+            // lblRecuperar
+            // 
+            this.lblRecuperar.AutoSize = true;
+            this.lblRecuperar.BackColor = System.Drawing.Color.Transparent;
+            this.lblRecuperar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblRecuperar.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecuperar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblRecuperar.Location = new System.Drawing.Point(116, 208);
+            this.lblRecuperar.Name = "lblRecuperar";
+            this.lblRecuperar.Size = new System.Drawing.Size(189, 22);
+            this.lblRecuperar.TabIndex = 3;
+            this.lblRecuperar.Text = "¿Olvidaste tu contraseña?";
+            // 
             // LoginView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(982, 603);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(734, 431);
+            this.Controls.Add(this.panelCredenciales);
             this.Controls.Add(this.lblSubtitulo);
             this.Controls.Add(this.lblTitulo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "LoginView";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panelCredenciales.ResumeLayout(false);
+            this.panelCredenciales.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,5 +176,12 @@
 
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Label lblSubtitulo;
+        private System.Windows.Forms.Panel panelCredenciales;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnLoggin;
+        private System.Windows.Forms.Label lblRecuperar;
     }
 }
